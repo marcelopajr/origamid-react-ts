be2e9283c56cde0c653540f4c7967d39f600ecf5
marcelopajr <marcelo96jr@gmail.com>
2023-08-23 17:35:14 -0300

    chore: props 1, 2 and 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
+import Input from "./Input";
+
 function App() {
-  return <div>Hello World</div>;
+  const [data, setData] = useState("");
+
+  return (
+    <div>
+      <p>Início da Viagem: {data}</p>
+      <Input id="email" label="Email" type="email" />
+      <Input id="nome" label="Nome" />
+      <Input
+        id="inicio"
+        label="Início da Viagem"
+        type="date"
+        value={data}
+        onChange={(event) => {
+          setData(event.currentTarget.value);
+        }}
+      />
+      <Input id="horario" label="Horário" type="time" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Input.tsx
@@ -0,0 +1,15 @@
+type InputProps = React.ComponentProps<"input"> & {
+  label: string;
+  id: string;
+};
+
+const Input = ({ label, id, ...props }: InputProps) => {
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} name={id} type="text" {...props} />
+    </div>
+  );
+};
+
+export default Input;
